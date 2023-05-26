0e1a5ec9b93d3fea9590ab543179820488a9f684
Dallin McLaughlin <nillad12@gmail.com>
2023-05-26 14:22:29 +1200

    Implemented quiz page

---
+++ server/routes/quizpage.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const db = require('../db/db')
+// const { path } = require('../server')
+
+const router = express.Router()
+
+router.get('/:id', async (req, res) => {
+  const template = 'quiz'
+  const titans = await db.getTitans()
+  const titanId = req.params.id
+  const titanIndex = titanId - 1
+  let difference = 0
+  //const titanObject = titans.find(titan => titan.id === titannpm Id);
+  console.log('hi: ', titans[titanIndex]['answer'])
+  console.log('hi: ', titanIndex)
+  console.log('image: ', titans[titanIndex].image)
+  const viewData = {
+    image: titans[titanIndex].image,
+    name: titans[titanIndex].name,
+    options: [
+      titans[titanIndex]['answer'],
+      titans[(titanIndex + 1) % titans.length]['answer'],
+      titans[(titanIndex + 2) % titans.length]['answer'],
+      titans[(titanIndex + 3) % titans.length]['answer'],
+    ],
+  }
+  res.render(template, viewData)
+})
+
+router.post('/:id', async (req, res) => {
+  const titanId = req.params.id
+  res.redirect('/:answer')
+})
+
+module.exports = router
+// '../public/cronus.png'
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const userRoutes = require('./routes/homepage')
+const quizRoutes = require('./routes/quizpage')
 
 const server = express()
 
@@ -9,9 +10,10 @@ const server = express()
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
+server.use(express.static(__dirname + '/public'))
 server.use(express.urlencoded({ extended: true }))
 
 // Routes
 server.use('/', userRoutes)
-
+server.use('/quiz', quizRoutes)
 module.exports = server
